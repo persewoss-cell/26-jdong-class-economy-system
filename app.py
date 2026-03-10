d35c0f887866ede33a1afe4300b8486b1da64743
persewoss-cell <persewoss@gmail.com>
2026-03-10 21:31:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13022,7 +13022,7 @@ div[data-testid="stElementContainer"]:has(.stat_bulk_text){
             with bulk_cols[0]:
                 st.markdown("<div class='stat_bulk_text'>&nbsp;</div>", unsafe_allow_html=True)
             with bulk_cols[1]:
-                st.markdown("<div class='stat_bulk_text'><b>일괄 적용버튼</b></div>", unsafe_allow_html=True)
+                st.markdown("<div class='stat_bulk_text'><b>⚡일괄 적용버튼⚡</b></div>", unsafe_allow_html=True)
             for j, sub in enumerate(sub_rows):
                 with bulk_cols[j + 2]:
                     sub_id = str(sub.get("submission_id", "") or "")
